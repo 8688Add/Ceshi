e02c264581315b5298aff0f09887fa491877f84c
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-09 19:24:49 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -154,7 +154,18 @@ if [ `grep -c "CONFIG_TARGET_x86_64=y" ${Home}/.config` -eq '1' ]; then
 else
 	TARGET_ADG="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
 fi
-if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then
+
+if [[ `grep -c "CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray=y" ${Home}/.config` -eq '1' ]]; then
+	sed -i 's/CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray=y/# CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray is not set/g' ${Home}/.config
+elif [[ `grep -c "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray=y" ${Home}/.config` -eq '1' ]]; then
+	sed -i 's/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray=y/# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray is not set/g' ${Home}/.config
+elif [[ `grep -c "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y" ${Home}/.config` -eq '1' ]]; then
+	sed -i 's/CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y/# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray is not set/g' ${Home}/.config
+else
+	echo
+fi
+
+if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then	
 	if [[ "${TARGET_ADG}" == "x86-64" ]];then
 		svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64 ${Home}/files
 		chmod -R 777 ${Home}/files/usr/bin/AdGuardHome
