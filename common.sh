dc9bad245b8acd7bd37064ebb39b91c39efa586a
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-09 12:05:11 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -6,9 +6,11 @@
 # 全脚本源码通用diy.sh文件
 Diy_all() {
 git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
+	if [[ ${REGULAR_UPDATE} == "true" ]]; then
 git clone https://github.com/281677160/luci-app-autoupdate package/luci-app-autoupdate
 mv "${PATH1}"/{AutoUpdate.sh,AutoBuild_Tools.sh} package/base-files/files/bin
 chmod -R +x package/base-files/files/bin
+fi
 }
 
 # 全脚本源码通用diy2.sh文件
