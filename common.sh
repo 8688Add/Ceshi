2097447e2d2ef202c4a5208278379dc06dc301c6
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-10 10:24:59 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -172,7 +172,7 @@ case "${REPO_URL}" in
 	if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then	
 		mkdir -p files/usr/bin
 		if [[ "${TARGET_ADG}" == "x86-64" ]];then
-			svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64 ${Home}/files
+			svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64/usr/bin ${Home}/files/usr/bin
 			chmod -R 777 ${Home}/files/usr/bin/AdGuardHome
 		fi
 		if [[ "${TARGET_ADG}" == "friendlyarm_nanopi-r2s" ]];then
