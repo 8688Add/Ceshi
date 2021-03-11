c82ad6b8f9990f12f9a0c4d44c4a7c7ba0ab4f69
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-11 22:29:12 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -82,6 +82,9 @@ echo
 rm -rf feeds/packages/net/adguardhome
 sed -i "/exit 0/i\sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm" package/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm" package/default-settings/files/zzz-default-settings
+if [[ `grep -c "CONFIG_PACKAGE_luci-app-passwall=y" ${Home}/.config` -eq '0' ]]; then
+	echo -e "\nCONFIG_PACKAGE_luci-app-passwall=y" >> "${Home}/.config"
+fi
 }
 
 ################################################################################################################
