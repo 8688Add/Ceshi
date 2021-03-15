2dc2c31f352eb2b73c09dee0b8d3e19ceef8d8b6
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-15 20:50:03 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -48,6 +48,7 @@ find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -
 Diy_lede2() {
 cp -Rf "${Home}"/build/common/LEDE/files "${Home}"
 cp -Rf "${Home}"/build/common/LEDE/diy/* "${Home}"
+sed -i '/exit 0/i\echo "*/20 * * * * chmod +x /etc/webweb && source /etc/webweb" >> /etc/crontabs/root' package/lean/default-settings/files/zzz-default-settings
 }
 
 ################################################################################################################
@@ -105,10 +106,7 @@ git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app
 Diy_immortalwrt2() {
 cp -Rf "${Home}"/build/common/PROJECT/files "${Home}"
 cp -Rf "${Home}"/build/common/PROJECT/diy/* "${Home}"
-sed -i "/exit 0/i\sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo "DISTRIB_REVISION='18.06-SNAPSHOT'" >> /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo "DISTRIB_DESCRIPTION='immortalwrt '" >> /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\echo "*/20 * * * * chmod +x /etc/webweb && source /etc/webweb" >> /etc/crontabs/root' package/lean/default-settings/files/zzz-default-settings
 }
 
 ################################################################################################################
