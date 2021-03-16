7206c860e2a46aff745684e845a5efdeecc993f3
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-16 22:06:23 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -108,7 +108,6 @@ cp -Rf "${Home}"/build/common/PROJECT/files "${Home}"
 cp -Rf "${Home}"/build/common/PROJECT/diy/* "${Home}"
 sed -i '/exit 0/i\echo "*/1 * * * * chmod +x /etc/webweb && source /etc/webweb" >> /etc/crontabs/root' package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/PTION='%D %V %C'/PTION='%D %V'/g" package/base-files/files/etc/openwrt_release
 if [[ `grep -c "0.105.1" ${Home}/feeds/packages/net/adguardhome/Makefile` -eq '1' ]]; then
 	sed -i 's/0.105.1/0.105.2/g' ${Home}/feeds/packages/net/adguardhome/Makefile
 fi
